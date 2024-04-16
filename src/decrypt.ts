091a52650decf515c7dbfba7addb11d18ef2d1ca
BlackWind001 <msanirudh1998@gmail.com>
2024-04-16 08:32:57 +0530

    Turned the decrypt function async.

--- src/decrypt.ts
+++ src/decrypt.ts
@@ -1,7 +1,7 @@
 // ToDo: Replace CryptoJS with either crypto module
 // or custom implementation of AES-128
 import CryptoJS from 'crypto-js';
-function decrypt (encryptedString: string, password: string) {
+async function decrypt (encryptedString: string, password: string) {
   const bytes = CryptoJS.AES.decrypt(encryptedString, password);
 
   return bytes.toString(CryptoJS.enc.Utf8);
