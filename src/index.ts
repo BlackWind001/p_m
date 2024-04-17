952f58d57d7dfeedadb3c370ef278d3bb7083ef5
BlackWind001 <msanirudh1998@gmail.com>
2024-04-17 11:10:04 +0530

    Added @inquirer/input package.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import input from '@inquirer/input';
+import passwordInput from '@inquirer/password';
 import encrypt from './encrypt';
 import decrypt from './decrypt';
 
 async function main () {
   const toEncrypt = await input({ message: 'Enter a string to encrpyt' });
-  const password = await input({ message: 'Enter password to encrypt the string' });
+  const password = await passwordInput({ message: 'Enter password to encrypt the string', mask: true });
 
   const encryptedText = <string>await encrypt(toEncrypt, password);
 
