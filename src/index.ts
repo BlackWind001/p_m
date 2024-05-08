a80a9811af765b3c9bfac820a0671f1ca7f834ff
Blackwind001 <msanirudh1998@gmail.com>
2024-05-09 01:19:19 +0530

    Removed dead code.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import input from '@inquirer/input';
-import passwordInput from '@inquirer/password';
-import encrypt from './encrypt';
-import decrypt from './decrypt';
 import { program } from 'commander';
 import initializeNewPasswordGitRepo from './initializeNewPasswordGitRepo';
 import setupTestPassword from './setupTestPassword';
@@ -13,19 +9,6 @@ import deleteExistingPassword from './deleteExistingPassword';
 import updateExistingPassword from './updateExistingPassword';
 import viewPassword from './viewPassword';
 import updateMasterPassword from './updateMasterPassword';
-
-async function encryptDecrypt () {
-  const toEncrypt = await input({ message: 'Enter a string to encrpyt' });
-  const password = await passwordInput({ message: 'Enter password to encrypt the string', mask: true });
-
-  const encryptedText = <string>await encrypt(toEncrypt, password);
-
-  console.log(encryptedText);
-
-  const decryptedText = await decrypt(encryptedText, password);
-
-  console.log(decryptedText);
-}
 
 async function main () {
 
